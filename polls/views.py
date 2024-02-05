2d44bb586c339e8f09c004c81db6851a501af18a
Veronica Ni <vni1294@gmail.com>
2024-02-05 15:32:05 -0500

    Added view functions to test routing

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def test(request):
+    return HttpResponse("Hello, world. You're at the polls test.")
+
+def test_user(request):
+    return HttpResponse("Hello, world. You're at the polls test_user.")
