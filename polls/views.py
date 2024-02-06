8257a183c0d2a430596cd8288bf7e643a504af1a
Veronica Ni <vni1294@gmail.com>
2024-02-05 21:11:47 -0500

    Added tests for IndexView and DetailView for hiding msgs with pub dates in future

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 # from django.template import loader
 from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 # def index(request):
@@ -27,8 +28,12 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date")[:5]
 
 # def detail(request, question_id):
 #     try:
@@ -45,6 +50,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
@@ -54,6 +65,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
